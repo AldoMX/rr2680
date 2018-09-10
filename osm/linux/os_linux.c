5ef9985f675ea81abb8cb2fa07f6d6dbe47054f2
Aldo Fregoso <aldomx@gmail.com>
2018-09-09 19:59:14 -0500

    Added support for kernel >= 4.8

--- osm/linux/os_linux.c
+++ osm/linux/os_linux.c
@@ -364,7 +364,11 @@ void refresh_sd_flags(PVBUS_EXT vbus_ext)
 						blkdev_get(bdev, FMODE_READ, 0 __BDEV_RAW)
 #endif
 						==0) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,8,0)
+						if (bdev->bd_disk && ((disk_to_dev(bdev->bd_disk)->parent)==&SDptr->sdev_gendev)) {
+#else
 						if (bdev->bd_disk && bdev->bd_disk->driverfs_dev==&SDptr->sdev_gendev) {
+#endif
 							if (vbus_ext->sd_flags[id] & SD_FLAG_REVALIDATE) {
 								if (bdev->bd_disk->fops->revalidate_disk)
 									bdev->bd_disk->fops->revalidate_disk(bdev->bd_disk);
